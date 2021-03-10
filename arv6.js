39c8385ed6d362c9e2454767430a41c93f7c3827
HungDinhGemPages <hungdinh@gempage.help>
2021-03-10 14:54:24 +0700

    update logic and style

--- arv6.js
+++
@@ -1,9 +0,0 @@
-// var data = { foo: 'bar' }
-// var eventData = new CustomEvent('GemARv6Event', { detail: data })
-// window.parent.document.dispatchEvent(eventData)
-
-
-(function () {
-  'use strict';
-  
-})();
--- gempage.js
+++ gempage.js
@@ -1,54 +1,33 @@
 
 (function (gd) {
   'use strict';
-  // window.document.addEventListener('GemARv6Event', handleEvent, false)
-  // function handleEvent(e) {
-  //   console.log(e.detail) // outputs: {foo: 'bar'}
-  // }
+  window.document.addEventListener('GemARv6Portal', receiverARv6Event, false)
+  function receiverARv6Event(event) {
+    displayReceivedData(event.detail);
+  }
+
   const $container = gd.getElementsByClassName("container");
   const $popup = gd.getElementById("popup-selecter");
   const $button = gd.getElementById("trigger-call-api");
- 
-  $button.addEventListener("click", async function () {
-    // $container.classList.remove("hide");
-    [].forEach.call($container, function(el) {
+
+  $button.addEventListener("click", function () {
+    [].forEach.call($container, function (el) {
       el.className = el.className.replace(/hide/, "");
     });
-    [].forEach.call($popup, function(el) {
+    [].forEach.call($popup, function (el) {
       el.className = el.className.replace(/hide/, "");
     });
-    // $popup.classList.remove("hide");
     $button.classList.add("disabled");
     $button.innerHTML = "Rendering";
 
-    const contentHTML = await getContentIframe();
     $button.classList.remove("disabled");
-    // $button.innerHTML = "Get List";
-    // var parser = new DOMParser();
-    // var doc = parser.parseFromString(contentHTML, "text/html");
-    // console.log("doc", JSON.parse(doc));
+    $button.innerHTML = "Get List";
   });
-  const getContentIframe = async () => {
-    try {
-      const result = await makeIframeContent();
-      console.log("type of result", typeof result)
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  // Fake Generate Data
-  const sectionsList = `
-  `
-  const makeIframeContent = () =>
-    new Promise((resolve, reject) => {
-      if (!sectionsList) {
-        return setTimeout(
-          () => reject(new Error('Section not found')),
-          250
-        );
-      }
 
-      setTimeout(() => resolve(sectionsList), 250);
-    });
+  const displayReceivedData = (data) => {
+    let displayStr = "Name: " + data.name + "<br>";
+    displayStr += "Anchor value: " + data.anchor;
+    gd.getElementById("displayer").innerHTML = displayStr;
+  }
+
 })(document);
